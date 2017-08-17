6eab7f246fc55625b090c44912ab83bd9352b099
mrAppleXZ <mr.applexz@gmail.com>
2017-08-17 10:28:12 +0500

    Umm...

--- src/main/java/ru/pearx/lib/Color.java
+++ src/main/java/ru/pearx/lib/Color.java
@@ -9,135 +9,59 @@ package ru.pearx.lib;
  */
 public class Color
 {
-    private byte red;
-    private byte green;
-    private byte blue;
-    private byte alpha;
+    private int argb;
 
-    /**
-     * Creates the new {@link Color} instance.
-     * @param red Red value.
-     * @param green Green value.
-     * @param blue Blue value.
-     * @param alpha Alpha value.
-     */
-    public Color(byte red, byte green, byte blue, byte alpha)
+    protected Color()
     {
-        this.red = red;
-        this.green = green;
-        this.blue = blue;
-        this.alpha = alpha;
+
     }
 
-
-    /**
-     * Creates the new {@link Color} from ARGB.
-     * @param argb ARGB value.
-     * @return The new {@link Color} instance.
-     */
     public static Color fromARGB(int argb)
     {
-        byte a = (byte)(argb >> 24 & 255);
-        byte r = (byte)(argb >> 16 & 255);
-        byte g = (byte)(argb >> 8 & 255);
-        byte b = (byte)(argb & 255);
-        return new Color(r, g, b, a);
-    }
-
-    /**
-     * Creates the new {@link Color} from RGB.
-     * @param rgb RGB value.
-     * @return The new {@link Color} instance.
-     */
-    public static Color fromRGB(int rgb)
-    {
-        Color col = Color.fromARGB(rgb);
-        col.setAlpha(255);
+        Color col = new Color();
+        col.setARGB(argb);
         return col;
     }
 
-    /**
-     * Converts this color to the ARGB int.
-     */
-    public int toARGB()
+    public static Color fromRGB(int rgb)
     {
-        int i = 0;
-        i |= getAlpha() << 24;
-        i |= getRed() << 16;
-        i |= getGreen() << 8;
-        i |= getBlue();
-        return i;
+        Color col = new Color();
+        col.setRGB(rgb);
+        return col;
     }
 
-    /**
-     * Converts this color to the RGB int.
-     */
-    public int toRGB()
+    public int getARGB()
     {
-        int i = 0;
-        i |= getRed() << 16;
-        i |= getGreen() << 8;
-        i |= getBlue();
-        return i;
+        return argb;
     }
 
-    public byte getRed()
+    public void setARGB(int argb)
     {
-        return red;
+        this.argb = argb;
     }
 
-    public void setRed(byte red)
+    public void setRGB(int rgb)
     {
-        this.red = red;
+        setARGB(rgb | 0xFF000000);
     }
 
-    public byte getGreen()
+    public int getAlpha()
     {
-        return green;
+        return (getARGB() >> 24) & 0xFF;
     }
 
-    public void setGreen(byte green)
+    public int getRed()
     {
-        this.green = green;
+        return (getARGB() >> 16) & 0xFF;
     }
 
-    public byte getBlue()
+    public int getGreen()
     {
-        return blue;
+        return (getARGB() >> 8) & 0xFF;
     }
 
-    public void setBlue(byte blue)
+    public int getBlue()
     {
-        this.blue = blue;
-    }
-
-    public byte getAlpha()
-    {
-        return alpha;
-    }
-
-    public void setAlpha(byte alpha)
-    {
-        this.alpha = alpha;
-    }
-
-    public void setRed(int red)
-    {
-        setRed((byte)red);
-    }
-
-    public void setGreen(int green)
-    {
-        setGreen((byte)green);
-    }
-
-    public void setBlue(int blue)
-    {
-        setBlue((byte)blue);
-    }
-
-    public void setAlpha(int alpha)
-    {
-        setAlpha((byte)alpha);
+        return getARGB() & 0xFF;
     }
 }
