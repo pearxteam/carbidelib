d6fdfd0dc758cb01d90e8d33a968cec8efe167ce
mrAppleXZ <mr.applexz@gmail.com>
2017-08-21 12:29:46 +0500

    Size

---
+++ src/main/java/ru/pearx/lib/Size.java
@@ -0,0 +1,36 @@
+package ru.pearx.lib;
+
+/*
+ * Created by mrAppleXZ on 21.08.17 12:28.
+ */
+public class Size
+{
+    private int width;
+    private int height;
+
+    public Size(int width, int height)
+    {
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getWidth()
+    {
+        return width;
+    }
+
+    public void setWidth(int width)
+    {
+        this.width = width;
+    }
+
+    public int getHeight()
+    {
+        return height;
+    }
+
+    public void setHeight(int height)
+    {
+        this.height = height;
+    }
+}
