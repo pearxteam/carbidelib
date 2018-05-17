1eb3da49e0039eb67d89ae7be0ca12f52055025c
mrAppleXZ <mr.applexz@gmail.com>
2018-05-17 21:43:30 +0500

    Version Comparison

---
+++ src/main/java/ru/pearx/carbide/VersionComparator.java
@@ -0,0 +1,54 @@
+package ru.pearx.carbide;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/*
+ * Created by mrAppleXZ on 17.05.18 16:24.
+ */
+public final class VersionComparator
+{
+    private static final Pattern PATTERN = Pattern.compile("(\\d+)|([A-Za-z]+)");
+
+    private VersionComparator() {}
+
+    public static int compare(String a, String b)
+    {
+        if(a.equals(b))
+            return 0;
+
+        Matcher matA = PATTERN.matcher(a);
+        Matcher matB = PATTERN.matcher(b);
+
+        for(boolean foundA = matA.find(), foundB = matB.find(); foundA || foundB; foundA = matA.find(), foundB = matB.find())
+        {
+            if(foundA && !foundB)
+                return 1;
+            if(!foundA && foundB)
+                return -1;
+            else
+            {
+                String aNumber = matA.group(1);
+                String aString = matA.group(2);
+                String bNumber = matB.group(1);
+                String bString = matB.group(2);
+                int result;
+                if(aNumber != null && bNumber != null)
+                {
+                    result = Integer.compare(Integer.parseInt(aNumber), Integer.parseInt(bNumber));
+                }
+                else if(aString != null && bString != null)
+                {
+                    result = aString.compareTo(bString);
+                }
+                else
+                {
+                    result = aNumber != null ? 1 : -1;
+                }
+                if(result != 0)
+                    return result;
+            }
+        }
+        return 0;
+    }
+}
---
+++ src/test/java/ru/pearx/carbide/tests/VersionComparatorTest.java
@@ -0,0 +1,36 @@
+package ru.pearx.carbide.tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+import ru.pearx.carbide.VersionComparator;
+
+import static org.junit.Assert.assertEquals;
+
+/*
+ * Created by mrAppleXZ on 17.05.18 21:10.
+ */
+public class VersionComparatorTest
+{
+    @Test
+    public void testEqualStrings()
+    {
+        assertEquals(0, VersionComparator.compare("1.2.75675", "1.2.75675"));
+    }
+
+    @Test
+    public void testEqualVersion()
+    {
+        assertEquals(0, VersionComparator.compare("1.2.6", "1-2-6"));
+        assertEquals(0, VersionComparator.compare("1.2.6b", "1-2-6b"));
+    }
+
+    @Test
+    public void testUnequalVersion()
+    {
+        assertEquals(-1, VersionComparator.compare("1.5.1", "1.5.2"));
+        assertEquals(1, VersionComparator.compare("1.6.1", "1.5.2"));
+        assertEquals(1, VersionComparator.compare("1.5.3", "1.5.a"));
+        assertEquals(-1, VersionComparator.compare("1.5.3", "1.5.3.4"));
+        assertEquals(1, VersionComparator.compare("1.5.3.1", "1.5.3"));
+    }
+}
